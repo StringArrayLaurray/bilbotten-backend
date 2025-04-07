e4a943f28ef0a661b7f2623349b32a1b1f7290a2
StringArrayLaurray <laan0004@stud.kea.dk>
2025-04-07 09:58:05 +0200

    Initial commit

---
+++ src/main/java/org/example/bilbottenbackend/BilbottenBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.bilbottenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilbottenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilbottenBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bilbottenbackend/BilbottenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bilbottenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilbottenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
