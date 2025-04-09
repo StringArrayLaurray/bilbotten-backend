c413b432bc1c9a48c830c253291e6c6af27adae2
Christoffer Lund <Christofferlund93@gmail.com>
2025-04-09 12:34:34 +0200

    extractPlateFromImage()

--- src/main/java/org/example/bilbottenbackend/controller/UploadController.java
+++ src/main/java/org/example/bilbottenbackend/controller/UploadController.java
@@ -2,9 +2,7 @@ package org.example.bilbottenbackend.controller;
 
 import org.example.bilbottenbackend.model.CarInfo;
 import org.example.bilbottenbackend.service.CarApiService;
-//import org.example.bilbottenbackend.service.GPTVisionService;
 import org.example.bilbottenbackend.service.GPTVisionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,23 +18,16 @@ public class UploadController {
     GPTVisionService gptVisionService;
     CarApiService carApiService;
 
-    @Autowired
     public UploadController(GPTVisionService gptVisionService, CarApiService carApiService) {
         this.gptVisionService = gptVisionService;
         this.carApiService = carApiService;
     }
 
    @PostMapping("/upload")
-    public ResponseEntity<String> upload(@RequestParam MultipartFile image){
-        String plate = gptVisionService.extractPlateFromImage(image).block(); // noget med at .block() skal ændres til flatMap() efter motorAPI kaldet er kodet.
-        return ResponseEntity.ok("Extracted: " + plate);
-
-    }
-
-    // til at teste med POSTMAN
-  @PostMapping("/uploadPOSTMAN")
-    public Mono<String> uploadPOSTMAN(@RequestParam MultipartFile image){
-        return gptVisionService.extractPlateFromImage(image);
+    public Mono<ResponseEntity<CarInfo>> upload(@RequestParam MultipartFile image){
+        return gptVisionService.extractPlateFromImage(image)
+                .flatMap(plate -> carApiService.getCarInfo(plate))
+                .map(carInfo -> ResponseEntity.ok(carInfo));
     }
 
     @GetMapping("/vehicles/{reg-no-or-vin}")
