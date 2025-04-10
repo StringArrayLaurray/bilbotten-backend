fea054c6a24d88c4142655559a397b0a71b973b7
StringArrayLaurray <laan0004@stud.kea.dk>
2025-04-10 10:58:30 +0200

    lortet virker nu. irri lombok

--- src/main/java/org/example/bilbottenbackend/model/AIResponse.java
+++ src/main/java/org/example/bilbottenbackend/model/AIResponse.java
@@ -11,16 +11,48 @@ import java.util.List;
 public class AIResponse {
     private List<Choice> choices;
 
-    @Getter
-    @Setter
+
     public static class Choice {
         private Message message;
+
+        public Message getMessage() {
+            return message;
+        }
+
+        public void setMessage(Message message) {
+            this.message = message;
+        }
     }
 
-    @Getter
-    @Setter
+    public List<Choice> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
+    }
+
+
     public static class Message {
         private String role;
         private String content;
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
     }
+
+
 }
